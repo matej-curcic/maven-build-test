33dd53b36963b8c792be679645c26650519074d5
Matej Curcic <matej.curcic@hcs-company.com>
2025-02-12 14:52:30 +0100

    Simple webserver

--- src/main/java/com/example/hello/Hello.java
+++ src/main/java/com/example/hello/Hello.java
@@ -1,9 +1,38 @@
-package com.example.hello;
+package com.example;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
 public class Hello {
-	
-	public static void main(String [] args) {
-		System.out.println("Hello, world!");
-	}
-	
+    public static void main(String[] args) throws IOException {
+        // Start a simple HTTP server
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+	server.createContext("/", new EchoHandler());
+
+        server.setExecutor(null); // Use default executor
+        server.start();
+        System.out.println("Server started on port 8080");
+    }
+
+    static class EchoHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+
+            exchange.sendResponseHeaders(200, 0);
+            OutputStream os = exchange.getResponseBody();
+
+	    os.write(exchange.getRequestURI().getPath().getBytes());
+
+            os.close();
+        }
+    }
 }
